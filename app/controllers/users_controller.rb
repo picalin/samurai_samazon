286d3eb751d6f1bdf483a9f8d274748a5999deec
EC2 Default User <ec2-user@ip-172-31-28-96.ap-northeast-1.compute.internal>
2021-11-08 02:08:07 +0000

    12 unsubscribed user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,12 @@ class UsersController < ApplicationController
   def favorite
     @favorites = @user.likees(Product)
   end
+  
+  def destroy
+    @user.deleted_flg = User.switch_flg(@user.deleted_flg)
+    @user.update(deleted_flg: @user.deleted_flg)
+    redirect_to mypage_users_url
+  end
 
   private
     def set_user
