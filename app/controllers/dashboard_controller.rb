3e1afe4787e0a3548a05a995968f614eafe34ba5
picalin <s.hikaru.2125@gmail.com>
2021-11-10 22:45:30 +0900

    15 sales page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,16 @@ class DashboardController < ApplicationController
     before_action :authenticate_admin!
     layout 'dashboard/dashboard'
     def index
+        @sort = params[:sort]
+        @sort_list = ShoppingCart.sort_list
 
+        if @sort == "month"
+            sales = ShoppingCart.get_monthly_sales
+        else
+            sales = ShoppingCart.get_daily_sales
+        end
+        
+        # 配列に対してkamnariを使用している
+        @sales = Kaminari.paginate_array(sales).page(params[:page]).per(15)
     end
 end
