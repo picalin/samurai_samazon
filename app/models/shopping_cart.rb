86cf17f66e5c56c1a14c7c78490d58815f312f34
picalin <s.hikaru.2125@gmail.com>
2021-11-13 16:07:15 +0900

    19 management

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -15,6 +15,9 @@ class ShoppingCart < ApplicationRecord
 #   ある月または日に購入されたカートの一覧を取得するスコープ
   scope :search_bought_carts_by_month, -> (month) { bought_carts.where(updated_at: month.all_month) }
   scope :search_bought_carts_by_day, -> (day) { bought_carts.where(updated_at: day.all_day) }
+  scope :search_carts_by_ids, -> (ids) { where("id LIKE ? ", "%#{ids}%") }
+  scope :search_bought_carts_by_ids, -> (ids) { bought_carts.search_carts_by_ids(ids) }
+  
   scope :sort_list, -> {
     {"日別": "daily", "月別": "month"}
   }
